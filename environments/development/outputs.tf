826e92774fe9eafd6763e6945cc1dc2320ad3dfd
Nilay Saxena <nilaysaxena94@gmail.com>
2025-07-14 16:56:00 +0530

    Update outputs.tf

--- environments/development/outputs.tf
+++ environments/development/outputs.tf
@@ -6,15 +6,15 @@ output "vnet_ids" {
   value = { for vnet, value in module.virtual_networks : vnet => value.id }
 }
 
-output "vm_ids" {
-  value = { for vm, value in module.vms : vm =>
-    {
-      id        = value.id
-      privateip = value.privateip
-      publicip  = value.publicip
-    }
-  }
-}
+#output "vm_ids" {
+#  value = { for vm, value in module.vms : vm =>
+#    {
+#      id        = value.id
+#      privateip = value.privateip
+#      publicip  = value.publicip
+#    }
+#  }
+#}
 
 output "sa_ids" {
   value = { for sa, value in module.storage_accounts : sa => value.id }
